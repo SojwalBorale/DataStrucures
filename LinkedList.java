88c18d3065666d23378d60ef416bf4609e26bef9
Sojwal Borale <sojwalborale@gmail.com>
2025-07-12 21:40:51 +0530

    added full linkedlist

--- LinkedList.java
+++ LinkedList.java
@@ -52,15 +52,47 @@ public class LinkedList {
         }
         System.out.println("null");
     }
+    public int get(int index) {
+        Node temp = head;
+        int i = 0;
+        while (temp != null) {
+            if (i == index) return temp.data;
+            temp = temp.next;
+            i++;
+        }
+        throw new IndexOutOfBoundsException("Index " + index + " out of range");
+    }
+
+    // Set value at index
+    public void set(int index, int value) {
+        Node temp = head;
+        int i = 0;
+        while (temp != null) {
+            if (i == index) {
+                temp.data = value;
+                return;
+            }
+            temp = temp.next;
+            i++;
+        }
+        throw new IndexOutOfBoundsException("Index " + index + " out of range");
+    }
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-      list.insert(5);
-      list.insert(6);
-      list.insert(7);
-      list.insert(8);
-      list.traverse();
-      list.delete(21);
-      list.traverse();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.traverse();
+
+        list.set(1, 25);  // change 20 to 25
+        list.traverse();
+
+        System.out.println("Element at index 2: " + list.get(2));
+
+        list.delete(1);   // delete 25
+        list.traverse();
+        list.delete(21);
+        list.traverse();
     }
 }
 
